24bf207a106ece430a255d7658f5ece9f6f7efcc
christ297 <christlearn834@gmail.com>
2025-04-16 18:23:48 +0000

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,5 @@
+// src/main.ts
+import { bootstrapApplication } from '@angular/platform-browser';
+import { BirthdayComponent } from './app/birthday.component';
+
+bootstrapApplication(BirthdayComponent);
